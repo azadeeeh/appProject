712d232d853ce5e160e853a71bbf4d21476e6bb1
Mobina <mobina.mb96@gmail.com>
2024-01-08 11:08:20 -0800

    defining the items in PostPage code

--- src/Components/Posts/PostsPage.js
+++ src/Components/Posts/PostsPage.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react"; /*importing react for creating react component, importing usestate is a react hook for handling states and useEffect is a react hook to handle side effects*/
 import PostFilter from "./PostFilter";
 import PostList from "./PostList";
 import CreatePost from "./CreatePost";
 import PostService from "../../Services/PostService";
 
-export default function PostsPage({ showAddPost }) {
+export default function PostsPage({ showAddPost }) { /*showAddPost is a prop which can be True or False*/
 
-    const [posts, setPosts] = useState([]);
+    const [posts, setPosts] = useState([]);{/*defining the states*/}
     const [filteredPosts, setFilteredPosts] = useState([]);
     const [categories,] = useState(['Crafts', 'Outdoor', 'Technology', 'Cooking',"Drawing and Painting",
     "Photography",
@@ -41,20 +41,20 @@ export default function PostsPage({ showAddPost }) {
     "Metalworking",
     "Fashion Design"]); // Example categories
 
-    useEffect(() => {
-        async function fetchPosts() {
-            await PostService.getAll()
-                .then(fetchedPosts => {
-                    setPosts(fetchedPosts);
+    useEffect(() => { {/*in case of fetching data, subscription, or manual DOM manipulation, we handle the states as below*/}
+        async function fetchPosts() { /*asynchronous operations*/
+            await PostService.getAll() /*pusue the execution of fetchPosts until the Promise returned by PostService.getAll() is resolved.*/
+                .then(fetchedPosts => {/*when the promise is resolved the fetch data will be as fetchedposts*/
+                    setPosts(fetchedPosts); {/*update the fetchPost by applying setposts and setfilteredposts on the featchdata*/}
                     setFilteredPosts(fetchedPosts);
                 }).catch((err) => {
                     console.error("Error happened while fetching posts: ", err.message)
                 })
         }
         fetchPosts();
-    }, []);
+    }, []); /*ensures that the effectState happens only once*/
 
-    const handleAddPost = async (newPost) => {
+    const handleAddPost = async (newPost) => { /*this function is for adding a new post and updating the added post by including the new post*/
         PostService.add(
             newPost
         ).then((addedPost) => {
@@ -65,7 +65,7 @@ export default function PostsPage({ showAddPost }) {
         })
     }
 
-    const filterPosts = (category) => {
+    const filterPosts = (category) => { /*is responsible for updating the state FilteredPost given the category*/
         if (!category || category === '') {
             setFilteredPosts(posts);
         } else {
@@ -77,10 +77,10 @@ export default function PostsPage({ showAddPost }) {
     return (
         <>
 
-            <PostFilter categories={categories} filterPosts={filterPosts} />
-            {showAddPost && <CreatePost addNewPost={handleAddPost} categories={categories} />}
+            <PostFilter categories={categories} filterPosts={filterPosts} /> {/*render postfilter component with two props: predefined categories and filteredposts function*/}
+            {showAddPost && <CreatePost addNewPost={handleAddPost} categories={categories} />} {/*conditional rendering of component CreatPost if showAddPost is True*/}
 
-            <PostList posts={filteredPosts} />
+            <PostList posts={filteredPosts} /> {/*rendering component post with the prop filteredPsots*/}
         </>
     )
 }
