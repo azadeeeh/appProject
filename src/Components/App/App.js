c1496714d50e6db2a3716f7ac3240a685a79caca
Mobina <mobina.mb96@gmail.com>
2024-01-08 09:43:03 -0800

    defining the functionality of each code in App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,8 @@
 import '../App/App.css';
 import CreateEvent from '../Events/CreateEvent';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';/*Routes is a parent componenet that wraps all the components and defined to find the routing structure of the application*/
 import Navbar from '../NavBar/navbar';
-import PostsPage from '../Posts/PostsPage';
+import PostsPage from '../Posts/PostsPage'; /*import the component postpages in order to either show the pages or create a new page*/
 import DraftsPage from '../Resources/DraftsPage';
 import LibraryPage from '../Resources/LibraryPage';
 import EventButton from '../Events/EventButton';
@@ -15,14 +15,14 @@ const App = () => {
         <EventButton />
       </header>
 
-      <hr className="hrStyle" />
+      <hr className="hrStyle" /> {/* */}
 
       {/* <EventButton /> */}
 
-      <Routes>
-        <Route path='/Drafts' element={<DraftsPage />} />
+      <Routes> {/*Routes is a parent componenet that wraps all the components and defined to find the routing structure of the application*/}
+        <Route path='/Drafts' element={<DraftsPage />} /> {/*if the user click on the Drafts page, render the DraftPage component that we imported above from Resources/DraftPage path*/}
         <Route path='/Library' element={<LibraryPage />} />
-        <Route path='/Hobbies' element={<PostsPage showAddPost={true} />} />
+        <Route path='/Hobbies' element={<PostsPage showAddPost={true} />} />  {/*if showAddPost is True, it goes to the Hobbies page otherwise goes to the home page*/}
         <Route path='/' element={<PostsPage showAddPost={false} />} />
         <Route path="/create-event" element={<CreateEvent />} />
       </Routes>
