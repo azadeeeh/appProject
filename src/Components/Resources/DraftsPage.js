687450aa6b02e1273009bb92f98ea12fb9678d94
Sina Jahromi <sina.jhrm@gmail.com>
2024-01-10 13:49:15 -0500

    + Some comments

--- src/Components/Resources/DraftsPage.js
+++ src/Components/Resources/DraftsPage.js
@@ -7,11 +7,21 @@ import ResourcesFilter from "./ResourcesFilter";
 
 export default function DraftsPage() {
 
+    // state for setting the list of resources 
     const [resources, setResources] = useState([]);
+    // state for setting the list of filtered resources 
     const [filteredResources, setFilteredResources] = useState([]);
+    // state for which is false by default. It is only true when the content is being loaded in the useEffect. It could be used to create a loading component in thn future! 
     const [isResourcesLoading, setIsResourcesLoading] = useState(false);
 
+    /* The `useEffect` hook is used to perform side effects in functional components. In this case, the
+    `useEffect` hook is used to fetch resources from the server and update the state variables
+    `resources`, `filteredResources`, and `isResourcesLoading` accordingly. */
     useEffect(() => {
+        /**
+         * The function fetches resources using ResourceService.getAll() and updates the state
+         * variables with the fetched resources.
+         */
         async function fetchResources() {
             setIsResourcesLoading(true);
             await ResourceService.getAll()
@@ -27,7 +37,15 @@ export default function DraftsPage() {
         fetchResources();
     }, []);
 
+    /**
+     * The function `handleAddResource` adds a new resource to a list of resources, with a timestamp
+     * and empty user rate IDs and rates.
+     * @param newResource - The `newResource` parameter is an object that represents the new resource
+     * to be added. It should have the following properties:
+     */
     const handleAddResource = async (newResource) => {
+        /* The line `newResource.date = new Date().getTime() / 1000;` is setting the `date` property of
+        the `newResource` object to the current timestamp in seconds. */
         newResource.date = new Date().getTime() / 1000;
         newResource.userRateIds = [];
         newResource.rates = [];
@@ -41,13 +59,25 @@ export default function DraftsPage() {
         })
     }
 
+    /**
+     * The function `handleRemoveResource` deletes a resource from a list and updates the list
+     * accordingly.
+     * @param res2Del - The parameter `res2Del` is an object representing the resource that needs to be
+     * removed. It has a property `id` which is used to identify the resource to be deleted.
+     */
     const handleRemoveResource = async (res2Del) => {
         ResourceService.delete(res2Del.id);
+        // Set resources after removing the deleted resource form the list
         setResources(resources.filter((res) => {
             return res.id !== res2Del.id;
         }))
     }
 
+    /**
+     * The function `filterResources` filters an array of resources based on a specified category.
+     * @param resourceCategory - The resourceCategory parameter is a string that represents the
+     * category of resources that you want to filter.
+     */
     const filterResources = (resourceCategory) => {
         if (!resourceCategory || resourceCategory === '') {
             setFilteredResources(resources);
--- src/Components/Resources/LibraryPage.js
+++ src/Components/Resources/LibraryPage.js
@@ -6,13 +6,36 @@ import categories from "../../Types/categories";
 
 export default function DraftsPage() {
 
+
+    /* `const [resources, setResources] = useState([]);` is declaring a state variable called
+    `resources` and a function to update that state variable called `setResources`. The initial
+    value of the `resources` state variable is an empty array `[]`. */
     const [resources, setResources] = useState([]);
+    /* `const [filteredResources, setFilteredResources] = useState([]);` is declaring a state variable
+    called `filteredResources` and a function to update that state variable called
+    `setFilteredResources`. The initial value of the `filteredResources` state variable is an empty
+    array `[]`. This state variable is used to store the filtered resources based on the selected
+    category in the `ResourcesFilter` component. */
     const [filteredResources, setFilteredResources] = useState([]);
+    /* `const [isResourcesLoading, setIsResourcesLoading] = useState(false);` is declaring a state
+    variable called `isResourcesLoading` and a function to update that state variable called
+    `setIsResourcesLoading`. The initial value of the `isResourcesLoading` state variable is
+    `false`. */
     const [isResourcesLoading, setIsResourcesLoading] = useState(false);
 
+    /* The `useEffect` hook is used to perform side effects in functional components. In this case, it
+    is used to fetch resources from the server and update the state variables `resources`,
+    `filteredResources`, and `isResourcesLoading`. */
     useEffect(() => {
         async function fetchResources() {
+            /* `setIsResourcesLoading(true);` is updating the state variable `isResourcesLoading` to
+            `true`. This is typically used to indicate that the resources are currently being
+            fetched or loaded, and can be used to show a loading spinner or display a loading
+            message to the user. */
             setIsResourcesLoading(true);
+            /* The code `await ResourceService.getAll().then(fetchedResources => { ... }).catch((err)
+            => { ... })` is fetching all the resources from MockAPI using the
+            `ResourceService.getAll()` method. */
             await ResourceService.getAll()
                 .then(fetchedResources => {
                     let filteredResources = fetchedResources.filter((res) => { return res.rates.length > 5 && ((res.rates.reduce((sum, prevRate) => { return sum + prevRate }, 0) / res.rates.length) >= 4.5) })
@@ -27,12 +50,23 @@ export default function DraftsPage() {
         fetchResources();
     }, []);
 
+    /**
+     * The function `handleRemoveResource` deletes a resource from a list of resources and updates the
+     * state.
+     * @param res2Del - The parameter `res2Del` is an object representing the resource to be removed.
+     * It should have an `id` property that uniquely identifies the resource.
+     */
     const handleRemoveResource = async (res2Del) => {
         ResourceService.delete(res2Del.id);
         setResources(resources.filter((res) => {
             return res.id !== res2Del.id;
         }))
     }
+    /**
+     * The function `filterResources` filters an array of resources based on a specified category.
+     * @param resourceCategory - The resourceCategory parameter is a string that represents the
+     * category of resources that you want to filter.
+     */
     const filterResources = (resourceCategory) => {
         if (!resourceCategory || resourceCategory === '') {
             setFilteredResources(resources);
